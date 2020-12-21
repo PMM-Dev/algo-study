d929acaf6df1c59e477f6af86f7079441a1c824e
sjmjys954646 <sjmjys954646@gmail.com>
2020-12-21 13:07:04 +0900

    fix : boj1138_greedy

--- algorithm/week3/sjmjys954646_PS/boj1138_greedy.cpp
+++ algorithm/week3/sjmjys954646_PS/boj1138_greedy.cpp
@@ -13,67 +13,17 @@
 using namespace std;
 
 int N;
-vector<int> selected;
 bool selectcheck[11];
 int arr[11];
-bool succeed = false;
-
-
-void check()
-{
-	for (int i = 1; i <= N; i++)
-	{
-		int count = 0;
-		for (int j = 0; j < N; j++)
-		{
-			if (selected[j] == i)
-				break;
-			else if (selected[j] > i)
-				count++;
-		}
-		if (count != arr[i - 1])
-			return;
-
-		if (i == N)
-		{
-			for (int j = 0; j < N; j++)
-			{
-				cout << selected[j] << " ";
-			}
-			succeed = true;
-		}
-	}
-}
-
-void select()
-{
-	if (succeed == true)
-		return;
-
-	if (selected.size() == N)
-	{
-		check();
-		return;
-	}
-
-	for (int i = 1; i <= N; i++)
-	{
-		if (selectcheck[i] == true)
-			continue;
-		selected.push_back(i);
-		selectcheck[i] = true;
-		select();
-		selected.pop_back();
-		selectcheck[i] = false;
-	}
-}
-
+int ans[11];
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
+	memset(ans, -1, sizeof(ans));
 
 	cin >> N;
 
@@ -82,7 +32,26 @@ int main()
 		cin >> arr[i];
 	}
 
-	select();
+	for (int i = 0; i < N; i++)
+	{
+		int count = 0;
+		for (int j = 0; j < N; j++)
+		{
+			if (count == arr[i] && ans[j] == -1)
+			{
+				ans[j] = i + 1;
+				break;
+			}
+
+			if (ans[j] == -1)
+				count++;
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << ans[i] << " ";
+	}
 
 	return 0;
 }
